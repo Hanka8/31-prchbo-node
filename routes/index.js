450cc82ccb3fff3d7397e0f10ce9800a02ed93c8
Hanka8 <hanka.maruskevicova@gmail.com>
2023-09-06 22:17:49 +0200

    web scraping script

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,41 @@
 var express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function scrapeWebsite() {
+    try {
+      // Load website
+      const response = await axios.get('https://kariera.army.cz/volna-mista');
+      const html = response.data;
+
+      // Parse HTML with Cheerio
+      const $ = cheerio.load(html);
+
+      // Find and filter job listings
+      const jobListings = $('li.views-row').filter((index, element) => {
+        const text = $(element).text();
+        return text.includes('123');
+      });
+
+      // Print filtered job listings
+      console.log('<ul>');
+      jobListings.each((index, element) => {
+        console.log($.html(element));
+      });
+      console.log('</ul>');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  scrapeWebsite();
+
   res.render('index', { title: '31. pluk' });
 });
 
